e0de024baf0b1fb5f05062d90b3d527c2befff43
D4SuCE <lol23012002@gmail.com>
2022-07-30 19:29:41 +0300

    Update

--- mtfind/src/mtfind.cpp
+++ mtfind/src/mtfind.cpp
@@ -14,16 +14,16 @@ mtfind::~mtfind()
 	futures.clear();
 }
 
-static std::mutex s_mutex;
 
-static void getLine(std::ifstream& file, mtfind* mtf)
+
+void mtfind::getLine(std::ifstream& file)
 {
 	std::lock_guard<std::mutex> lock(s_mutex);
 	std::string line;
 	std::getline(file, line);
-	for (auto it = line.find(mtf->pattern); it != std::string::npos; it = line.find(mtf->pattern, ++it))
+	for (auto it = line.find(pattern); it != std::string::npos; it = line.find(pattern, ++it))
 	{
-		size_t i = it, j = it + mtf->pattern.size() - 1;
+		size_t i = it, j = it + pattern.size() - 1;
 
 		while (line[i] != ' ' && i != 0)
 		{
@@ -33,9 +33,9 @@ static void getLine(std::ifstream& file, mtfind* mtf)
 		{
 			j++;
 		}
-		mtf->finded.push_back({ mtf->row, (i > 0 ? i + 2 : i), line.substr((line[i] == ' ' ? i + 1 : i), j - i) });
+		finded.push_back({ row, (i > 0 ? i + 2 : i), line.substr((line[i] == ' ' ? i + 1 : i), j - i) });
 	}
-	mtf->row++;
+	row++;
 }
 
 void mtfind::find()
@@ -50,7 +50,7 @@ void mtfind::find()
 
 	while (file.good())
 	{
-		futures.push_back(std::async(std::launch::async, getLine, ref(file), this));
+		futures.push_back(std::async(std::launch::async, &mtfind::getLine, this, ref(file)));
 	}
 
 	file.close();
--- mtfind/src/mtfind.h
+++ mtfind/src/mtfind.h
@@ -17,17 +17,24 @@ struct Line
 class mtfind
 {
 public:
+	mtfind();
+	mtfind(std::string path, std::string pattern);
+	~mtfind();
+	
+
+private:
+	std::mutex s_mutex;
+	std::vector<std::future<void>> futures;
+
+private:
+	void getLine(std::ifstream& file);
+
+public:
 	std::string path;
 	std::string pattern;
 	std::vector<Line> finded;
 	size_t row = 1;
-
-private:
-	std::vector<std::future<void>> futures;
-
+	
 public:
-	mtfind();
-	mtfind(std::string path, std::string pattern);
-	~mtfind();	
 	void find();
 };
